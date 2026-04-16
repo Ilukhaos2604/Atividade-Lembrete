35aa03e2c4cdc86b803fe7c2def9bfa6174ee33e
Ilukhaos2604 <klavi@tutamail.com>
2026-04-15 21:36:07 -0300

    Código pronto

--- script.js
+++ script.js
@@ -72,13 +72,13 @@ function menuRegistrarLembrete() {
     Nome/Lembrete
     .: `,
       (usuario) => {
-        const nome = usuario;
+        const nome = usuario.toString(``);
         rl.question(
           `
     Prazo
     .: `,
           (usuario) => {
-            const prazo = usuario;
+            const prazo = usuario.toString(``);
             const lembrete = {
               nomeLembrete: nome,
               prazoLembrete: prazo,
@@ -121,13 +121,111 @@ Você gostaria de listar os lembretes registrados?
     Lembretes registrados e seus IDs:
     
     ${ID + 1}: 
-    nome: ${lembrete.nome}
-    prazo: ${lembrete.prazo}
+    nome: ${lembretes[ID].nomeLembrete}
+    prazo: ${lembretes[ID].prazoLembrete}
     concluído?: ${lembrete.conclusao === false ? `Não concluído` : `Concluído`}
     `);
         });
       }
       menu();
+    }
+  );
+}
+
+function menuConcluirLembrete() {
+  rl.question(
+    `  
+  Gostaria de concluir algum lembrete?
+  (s/n).: `,
+    (usuario) => {
+      const usuariom = usuario.toString(``);
+      switch (usuariom) {
+        case `s`:
+          lembretes.forEach((nome, ID) => {
+            console.log(`${ID + 1}: 
+  nome: ${lembretes[ID].nomeLembrete}
+  prazo: ${lembretes[ID].prazoLembrete}
+  concluído?: ${nome.conclusao === false ? `Não concluído` : `Concluído`}`);
+          });
+          rl.question(
+            `Segundo a lista em cima, selecione o ID(número ao lado) do lembrete que você gostaria de concluir
+  .: `,
+            (usuarion) => {
+              const escolhaUsu = parseInt(usuarion - 1);
+              lembretes.forEach((nome, ID) => {
+                if (ID === escolhaUsu) {
+                  lembretes[ID].conclusao = true;
+                }
+              });
+              menu();
+            }
+          );
+          break;
+        default:
+          console.log(`O Lembrete com este ID não existe!`);
+          menu();
+          break;
+      }
+    }
+  );
+}
+
+function menuEditarLembrete() {
+  rl.question(
+    `  
+  Gostaria de editar algum lembrete?
+  (s/n).: `,
+    (usuario) => {
+      const usuariom = usuario.toString(``);
+      switch (usuariom) {
+        case `s`:
+          lembretes.forEach((nome, ID) => {
+            console.log(`${ID + 1}: 
+  nome: ${lembretes[ID].nomeLembrete}
+  prazo: ${lembretes[ID].prazoLembrete}
+  concluído?: ${nome.conclusao === false ? `Não concluído` : `Concluído`}`);
+          });
+          rl.question(
+            `Segundo a lista em cima, selecione o ID(número ao lado) do lembrete que você gostaria de editar
+  .: `,
+            (usuarion) => {
+              const escolhaUsu = parseInt(usuarion) - 1;
+              rl.question(
+                `
+          Novo Nome/Lembrete
+          .: `,
+                (novoNomeLembrete) => {
+                  const nome = novoNomeLembrete.toString(``);
+                  rl.question(
+                    `
+          Novo Prazo
+          .: `,
+                    (novoPrazoLembrete) => {
+                      const prazo = novoPrazoLembrete.toString(``);
+
+                      lembretes[escolhaUsu].nomeLembrete = nome;
+                      lembretes[escolhaUsu].prazoLembrete = prazo;
+
+                      console.log(`
+              
+              Lembrete editado com sucesso!
+              
+              `);
+
+                      menu();
+                    }
+                  );
+                }
+              );
+            }
+          );
+          menu();
+          break;
+        default:
+          console.log(`O Lembrete com este ID não existe!`);
+          menu();
+          break;
+      }
     }
   );
 }
